1296539084ffcf0ec90dea3b4cbafa2d54962161
gguex <guillaume.guex@gmail.com>
2022-02-08 17:38:33 +0100

    Graph created

--- 3_wv_test.py
+++ 3_wv_test.py
@@ -8,9 +8,11 @@ from gensim.models import KeyedVectors
 import os
 from sklearn.metrics.pairwise import cosine_similarity
 import networkx as nx
+import matplotlib.pyplot as plt
+import matplotlib as mpl
 
 # Corpus tsv path
-corpus_tsv_path = "corpora/Romeo&Juliet.tsv"
+corpus_tsv_path = "corpora/Hamlet.tsv"
 
 # Global or per_act
 global_view = True
@@ -21,7 +23,7 @@ global_view = True
 corpus_df = pd.read_csv(corpus_tsv_path, sep="\t", index_col=0)
 
 # Make the list of important characters
-min_char_count = 5
+min_char_count = 10
 sent_char_count = corpus_df.groupby(["char_from"]).size()
 char_list = sent_char_count[sent_char_count > min_char_count].index
 
@@ -139,12 +141,78 @@ interact_polarity_df = pd.DataFrame({"polarity": interaction_polarity_score}, in
 
 # second choice
 
-direction_vector_2 = wv_model.get_vector("hatred")
-interaction_polarity_score_2 = cosine_similarity(np.asarray((direction_vector - direction_vector_2).reshape(1, -1)),
-                                                 np.asarray(interact_vectors))[0]
-
-interact_polarity_df_2 = pd.DataFrame({"polarity": interaction_polarity_score_2}, index=act_interact_thr_name_list)
+# direction_vector_2 = wv_model.get_vector("hatred")
+# interaction_polarity_score_2 = cosine_similarity(np.asarray((direction_vector - direction_vector_2).reshape(1, -1)),
+#                                                  np.asarray(interact_vectors))[0]
+#
+# interact_polarity_df_2 = pd.DataFrame({"polarity": interaction_polarity_score_2}, index=act_interact_thr_name_list)
 
 # --- Recreate interactions
 
-for act_interact_thr_list
+edge_weights = np.sum(dt_thr_matrix / 70, axis=1).T.tolist()[0]
+edge_weights_strength = np.sum(dt_thr_matrix, axis=1).T.tolist()[0]
+
+# Make the two vectors of interactions
+from_char_list = []
+to_char_list = []
+for act_interact in act_interact_thr_list:
+    from_char_list.append(act_interact[0])
+    to_char_list.append(act_interact[1])
+
+# Make the graph df
+graph_df = pd.DataFrame({"from_char": from_char_list, "to_char": to_char_list,
+                         "polarity": interaction_polarity_score,
+                         "weight": edge_weights})
+# Make the graph
+G = nx.from_pandas_edgelist(graph_df,
+                            source="from_char",
+                            target="to_char",
+                            edge_attr=True,
+                            create_using=nx.DiGraph())
+
+# the positions of nodes for the graph
+node_pos = nx.spring_layout(G, weight="weight")
+
+# Computing mean polarity of nodes
+node_polarity = []
+node_weight = []
+for node in G.nodes:
+    char_df = graph_df[graph_df["from_char"] == node]
+    node_polarity.append(sum(char_df["polarity"] * char_df["weight"]) / char_df["weight"].sum())
+    node_weight.append(char_df["weight"].sum() * 100)
+
+# cmap color
+cmap = plt.cm.coolwarm
+
+# Create the nodes
+nodes = nx.draw_networkx_nodes(G,
+                               node_size=node_weight,
+                               pos=node_pos,
+                               label=from_char_list,
+                               node_color=node_polarity,
+                               cmap=cmap,
+                               vmin=min(interaction_polarity_score),
+                               vmax=max(interaction_polarity_score))
+# Draw labels
+labels = nx.draw_networkx_labels(G, pos=node_pos, font_size=10)
+# Create the edges
+edges = nx.draw_networkx_edges(G,
+                               pos=node_pos,
+                               arrows=True,
+                               width=[G[u][v]["weight"] for u, v in G.edges()],
+                               node_size=node_weight,
+                               edge_color=[G[u][v]["polarity"] for u, v in G.edges()],
+                               edge_cmap=cmap,
+                               alpha=[G[u][v]["polarity"] for u, v in G.edges()],
+                               edge_vmin=min(interaction_polarity_score),
+                               edge_vmax=max(interaction_polarity_score),
+                               connectionstyle="arc3,rad=0.2")
+
+pc = mpl.collections.PatchCollection(edges, cmap=cmap)
+pc.set_array(interaction_polarity_score.tolist())
+
+
+ax = plt.gca()
+plt.colorbar(pc, ax=ax)
+ax.set_axis_off()
+plt.show()
