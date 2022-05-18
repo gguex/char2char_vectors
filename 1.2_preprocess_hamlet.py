de13e942d5b5744a3887e8a5006aadb98c89bfbb
gguex <guillaume.guex@gmail.com>
2022-05-18 17:02:11 +0200

    begin new preprocessing of hamlet

--- 1.2_preprocess_hamlet.py
+++ 1.2_preprocess_hamlet.py
@@ -15,69 +15,50 @@ output_tsv_path = "corpora/Hamlet/Hamlet.tsv"
 
 # Loading the corpus
 with open(input_corpus_path) as corpus_file:
-    corpus_txt = corpus_file.read()
+    corpus_lines = corpus_file.readlines()
 
-# Splitting into acts
-corpus_act_list = corpus_txt.split("ACT")
-# Removing the beggining
-corpus_act_list = corpus_act_list[6:]
+# Detect character
+act_presences = []
+scene_presences = []
+speaking_characters = []
+speaking_lines = []
+entrances = []
+actions = []
+for id_line, line in enumerate(corpus_lines):
 
-# Splitting into scenes
-corpus_actscene_llist = []
-for corpus_act in corpus_act_list:
-    corpus_scene_list = corpus_act.split("SCENE")
-    corpus_actscene_llist.append(corpus_scene_list[1:])
+    # Detect act or scene
+    act_bool = ("ACT" in line)
+    scene_bool = ("SCENE" in line)
+    # Detect entrance
+    entrance_detect = re.search("^Enter ", line)
+    # Detect action
+    action_detect = re.search("^\[\_", line)
+    # Detect character
+    char_detect = re.search("[A-Z `'’]{3,}\.", line)
 
-# Making a function for the treatment of sentences
-def sentence_treatment(sent):
-    # Remove "appartées"
-    processed_sent = re.sub("\[.+]", " ", sent)
-    # Remove EOL
-    processed_sent = re.sub("\n", " ", processed_sent)
-    # Remove extra spaces
-    processed_sent = re.sub(" +", " ", processed_sent).strip()
-    # Return result
-    return processed_sent
+    # Potential adding
+    speaking_character = ""
+    speaking_line = ""
+    entrance = ""
+    action = ""
 
+    # Check if entrance
+    if entrance_detect is not None:
+        entrance = line[entrance_detect.span()[1]:]
+    # Check if action
+    elif action_detect is not None:
+        action = line[action_detect.span()[1]:-3]
+    elif (not act_bool) and (not scene_bool):
+        if char_detect is not None:
+            speaking_character = char_detect.group()[:-1]
+            speaking_line = line[char_detect.span()[1]:]
+        else:
+            speaking_line = line
 
-# Making the dataframe for the corpus
-corpus_df = pd.DataFrame(columns=["act", "scene", "char_from", "char_to", "sentence"])
-
-# Looping of act and scene
-for id_act, corpus_act in enumerate(corpus_actscene_llist):
-    for id_scene, scene in enumerate(corpus_act):
-        # Removing scene number
-        if re.search("I\.", scene) is not None:
-            scene = scene[re.search("I\.", scene).end():]
-        elif re.search("V\.", scene) is not None:
-            scene = scene[re.search("V\.", scene).end():]
-        last_end = None
-        char_list, sent_list = [], []
-        # Looping on character
-        for expr in re.finditer("[A-Z `'’]{3,}\.", scene):
-            # Adding character name
-            char_list.append(expr.group(0)[:-1].strip())
-            if last_end is not None:
-                sent = scene[last_end:expr.start()]
-                # Adding the sentence
-                sent_list.append(sentence_treatment(sent))
-            last_end = expr.end()
-        sent = scene[last_end:]
-        # Adding the last sentence
-        sent_list.append(sentence_treatment(sent))
-
-        # Making the "char_to" vector
-        char_to = []
-        for id_char, char_from in enumerate(char_list):
-            rev_list = [char for char in char_list[id_char::-1] if char != char_from]
-            if len(rev_list) == 0:
-                char_to.append([char for char in char_list if char != char_from][0])
-            else:
-                char_to.append(rev_list[0])
-
-        # Making the scene dataframe
-        scene_df = pd.DataFrame({"act": [id_act+1]*len(sent_list), "scene": [id_scene+1]*len(sent_list),
-                                 "char_from": char_list, "char_to": char_to, "sentence": sent_list})
-        corpus_df = pd.concat([corpus_df, scene_df], ignore_index=True)
-
-corpus_df.to_csv(output_tsv_path, sep="\t")
+    # Save results
+    act_presences.append(act_bool)
+    scene_presences.append(scene_bool)
+    speaking_characters.append(speaking_character)
+    speaking_lines.append(speaking_line)
+    entrances.append(entrance)
+    actions.append(action)
