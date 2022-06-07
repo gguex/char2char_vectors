af9f7f43312951e9900af09f7a624b4e23d3de05
gguex <guillaume.guex@gmail.com>
2022-06-07 12:32:53 +0200

    update codes according the n_tokens

--- 3.0_WV_lesMiserables.py
+++ 3.0_WV_lesMiserables.py
@@ -79,7 +79,7 @@ corpus.remove_occurrences_with_frequency(1e-10)
 corpus.remove_words_with_frequency(1e-10)
 
 # Get units weights
-f_row = (corpus.units_words.sum(axis=1) / corpus.units_words.sum().sum()).to_numpy()
+f_row = corpus.n_tokens / corpus.n_tokens.sum()
 
 # -------------------------------
 #  Analysis
